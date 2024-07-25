310cf4b52d5b15f130d0befc42b270865f56f077
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-07-26 00:22:06 +0530

    Added Time Widget

--- src/Components/SlidesWidget/SlidesWidget.jsx
+++ src/Components/SlidesWidget/SlidesWidget.jsx
@@ -3,9 +3,18 @@ import './SlidesWidget.css';
 
 const SlidesWidget = () =>
 {
+    let now = new Date();
+    let date = now.toDateString();
+    let time = now.toTimeString(); 
     return(
         <div className="slidesWidget">
-            <iframe className="slides-iframe" src="https://docs.google.com/presentation/d/e/2PACX-1vR2j9xR1FhEfiWSXWWRcehjwR0NjmlMMLh8940QXFFiK-y9RH5nRLswrUGpESpiMF0tpQXcAWSFVTI_/embed?start=true&loop=true&delayms=3000" frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+            <div className="date-time">
+                <span className="time">{time}</span><br></br>
+                <span className="date">{date}</span>
+            </div>
+            <div className="iframe-wrapper">
+                <iframe className="slides-iframe" src="https://docs.google.com/presentation/d/e/2PACX-1vR2j9xR1FhEfiWSXWWRcehjwR0NjmlMMLh8940QXFFiK-y9RH5nRLswrUGpESpiMF0tpQXcAWSFVTI_/embed?start=true&loop=true&delayms=3000" frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+            </div>
         </div>
     )
 }
