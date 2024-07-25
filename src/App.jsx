d18431de1ca8e88455d1c888e8992a7771a99ab4
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-07-25 14:53:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SlidesWidget from './Components/SlidesWidget/SlidesWidget;'
+
+const App = () =>
+{
+  return(
+    <div className="App">
+      <SlidesWidget/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/SlidesWidget/SlidesWidget.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SlidesWidget = ()=>
+{
+    return(
+        <div className="slidesWidget">
+            
+        </div>
+    )
+}
+
+export default SlidesWidget;
