35f896312d058348c870d8e1978b1e90bb8b1939
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-07-26 01:19:05 +0530

    Added styling to widgets

---
+++ src/Components/DGCWidget/DGCWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './DGCWidget.css';
+
+const DGCWidget = () =>
+{
+    return(
+        <div className="dgc">
+            
+        </div>
+    )
+}
+
+export default DGCWidget;
--- src/Components/SlidesWidget/SlidesWidget.jsx
+++ src/Components/SlidesWidget/SlidesWidget.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './SlidesWidget.css';
 
 const SlidesWidget = () =>
 {
-    let now = new Date();
-    let date = now.toDateString();
-    let time = now.toTimeString(); 
+    const [currentTime, setCurrentTime] = useState('');
+    const [timeZone, setTimeZone] = useState('');
+    const UpdateTime = () =>
+    {
+        let now = new Date();
+        let time = now.toTimeString().slice(0, 8); 
+        setCurrentTime(time);
+        setTimeZone(now.toTimeString().slice(8));
+    }
+
+    setTimeout(UpdateTime, 1000);
     return(
         <div className="slidesWidget">
             <div className="date-time">
-                <span className="time">{time}</span><br></br>
-                <span className="date">{date}</span>
+                <span className="time">{currentTime}</span>
+                <span className="time-zone">{timeZone}</span>
             </div>
             <div className="iframe-wrapper">
                 <iframe className="slides-iframe" src="https://docs.google.com/presentation/d/e/2PACX-1vR2j9xR1FhEfiWSXWWRcehjwR0NjmlMMLh8940QXFFiK-y9RH5nRLswrUGpESpiMF0tpQXcAWSFVTI_/embed?start=true&loop=true&delayms=3000" frameborder="0" width="480" height="299" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
