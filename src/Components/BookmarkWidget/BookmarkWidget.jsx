67846e6b516a3ba59006bc96e18758a480422b25
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-07-26 08:20:52 +0530

    Bookmarks widget added
    Google services bookmarks widget added. Currently only linked to their landing pages.

--- src/Components/BookmarkWidget/BookmarkWidget.jsx
+++ src/Components/BookmarkWidget/BookmarkWidget.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import './BookmarkWidget.css';
 import gmeet_icon from '../../assets/google-meet-svgrepo-com.svg';
+import docs_icon from '../../assets/google-docs-icon-2.svg';
+import keep_icon from '../../assets/google-keep-icon.svg';
 
 const BookmarkWidget = () =>
 {
     return(
         <div className="bookmarkWidget">
-            <a href="https://meet.google.com/landing" className="meetLink">Google Meet <img src={gmeet_icon} alt="" /></a>
+            <a href="https://meet.google.com/landing" className="meetLink"><img src={gmeet_icon} alt="G Meet" />Meet</a>
+            <a href="https://www.google.com/docs/about/" className="docsLink"><img src={docs_icon} alt="Docs" />Docs</a>
+            <a href="https://keep.google.com/" className="keepLink"><img src={keep_icon} alt="Keep" />Keep</a>
         </div>
     )
 }
